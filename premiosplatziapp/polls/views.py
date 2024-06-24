47f801f2c73d10bf9d9cbad0eba2a948a0239be6
kchavarriawork <kchavarriawork@gmail.com>
2024-06-24 15:13:20 -0600

    Agregando Templates y modificando el View

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-#Se importa el modulo HttpResponse para devolver una respuesta http simple.
+#Se importa el modulo HttpResponse para devolver una respuesta http simple.}
+
+from polls.models import Question
 
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse("Pagina principal de Premios Platzi")
+    question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "question_list": question_list
+    })
 
 
 def details(request, question_id):

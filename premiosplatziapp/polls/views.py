3a9f058c82d09caa34315e2d272389d464a213b2
kchavarriawork <kchavarriawork@gmail.com>
2024-06-26 18:39:22 -0600

    Creacion de la parte de results

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse 
+from django.http import HttpResponse, HttpResponseRedirect
 #Se importa el modulo HttpResponse para devolver una respuesta http simple.}
 
 from polls.models import Question
+from polls.models import Choice
+from django.urls import reverse
+
 
 
 # Create your views here.
@@ -21,8 +24,21 @@ def details(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"Estas viendo los resultados de la pregunta número: {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    context = {"question" : question}
+    return render(request, "polls/results.html", context)
 
 
 def vote(request, question_id):
-    return HttpResponse(f"Estas votando a la pregunta número: {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    context = {"question": question, 
+               "error_message": "No elegiste una respuesta"}
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/details.html", context)
+
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))

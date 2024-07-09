f1026b7c3af8bd6950a2b2328f2da995c3d0437d
kchavarriawork <kchavarriawork@gmail.com>
2024-07-09 15:08:30 -0600

    Se agregaron filtros al admin de Questions y estilos a index.html

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -5,6 +5,27 @@ from polls.models import Question
 from .models import Choice
 
 
+#Clase que permite agregar las respuestas de las preguntas
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+#Clase que define el orden de las preguntas
+class QuestionAdminOrder(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInline]
+
+    #Agrega y ordena los campos de Questions
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    
+    #Agrega un filtrado por el campo pub_date
+    list_filter = ["pub_date"]
+
+    #Agrega una barra de búsqueda y se filtrará por la pregunta
+    search_fields = ["question_text"]
+
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdminOrder)
 admin.site.register(Choice)
--- premiosplatziapp/premiosplatziapp/settings.py
+++ premiosplatziapp/premiosplatziapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# URL para servir archivos estáticos
 STATIC_URL = 'static/'
 
 # Default primary key field type
